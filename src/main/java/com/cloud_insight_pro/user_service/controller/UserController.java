21e8aa2e2763afb559c08f1c1dcf60ecb9bd06ac
ericndungutse <dav.ndungutse@gmail.com>
2025-09-04 10:37:21 +0200

    logut feature

--- src/main/java/com/cloud_insight_pro/user_service/controller/UserController.java
+++ src/main/java/com/cloud_insight_pro/user_service/controller/UserController.java
@@ -3,6 +3,8 @@ package com.cloud_insight_pro.user_service.controller;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,6 +23,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
   private final UserService userService;
+  @Value("${OAUTH_ISSUER_URL}")
+  private String OAUTH_ISSUER_URL;
 
   @GetMapping("/me")
   public ResponseEntity<APIResponse> getMe(@RequestHeader("X-User-Id") String userIdFromHeader) {
@@ -42,6 +46,22 @@ public class UserController {
         .success("User service is up and running", Map.of());
 
     return ResponseEntity.ok(response);
+  }
+
+  // Logout
+  @GetMapping("/logout")
+  public ResponseEntity<APIResponse> signout() {
+
+    // expiration
+    HttpHeaders headers = new HttpHeaders();
+    headers.add(HttpHeaders.SET_COOKIE, "token=; HttpOnly; Path=/; Max-Age=0; SameSite=lax; Secure");
+
+    APIResponse response = APIResponseFactory.success("Logged out successfully",
+        Map.of("logoutUrl", OAUTH_ISSUER_URL + "/logout"));
+
+    return ResponseEntity.ok()
+        .headers(headers)
+        .body(response);
   }
 
 }
